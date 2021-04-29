e7d82e3e4ba05937c16265a01a4ea0e39bdfd638
Alberto Rios de la Rosa <alu0101235929@ull.edu.es>
2021-04-29 19:06:11 +0100

    Ejercicio 4 completado

--- src/ejercicio4.ts
+++ src/ejercicio4.ts
@@ -6,18 +6,38 @@ import {spawn} from 'child_process';
 export class App4 {
   constructor() {}
 
-  comando1() {
-
+  comandoShow(fichero: string) {
+    fs.access(fichero, (err) => {
+      if (err) {
+        console.log(chalk.red.inverse(`La ruta introducida ${fichero} no existe o es incorrecta`));
+      } else {
+        fs.readFile(fichero, (err) => {
+          if (err) {
+            console.log(`La ruta introducida ${fichero} es un directorio`);
+          } else {
+            console.log(`La ruta introducida ${fichero} es un fichero`);
+          }
+        });
+      }
+    });
   }
 
-  comando2() {
-
+  comandoMkdir(directorio: string) {
+    fs.access(directorio, (err) => {
+      if (!err) {
+        console.log(chalk.red.inverse(`La ruta introducida ${directorio} ya existe`));
+      } else {
+        const mkdir = spawn(`mkdir`, [directorio]);
+        mkdir.stdout.pipe(process.stdout);
+        console.log(`${directorio} ha sido creado correctamente`);
+      }
+    });
   }
 
   comandoLs(directorio: string) {
     fs.access(directorio, (err) => {
       if (err) {
-        console.log(chalk.red(`La ruta introducida ${directorio} no existe o esta incorrecta`));
+        console.log(chalk.red.inverse(`La ruta introducida ${directorio} no existe o es incorrecta`));
       } else {
         const ls = spawn(`ls`, [directorio]);
         ls.stdout.pipe(process.stdout);
@@ -25,20 +45,87 @@ export class App4 {
     });
   }
 
-  comando4() {
-
+  comandoCat(directorio: string) {
+    fs.access(directorio, (err) => {
+      if (err) {
+        console.log(chalk.red.inverse(`La ruta introducida ${directorio} no existe o es incorrecta`));
+      } else {
+        fs.readFile(directorio, (err) => {
+          if (err) {
+            console.log(chalk.red.inverse(`La ruta introducida ${directorio} no es un fichero`));
+          } else {
+            const cat = spawn(`cat`, [directorio]);
+            cat.stdout.pipe(process.stdout);
+          }
+        });
+      }
+    });
   }
 
-  comando5() {
-
+  comandoRm(directorio: string) {
+    fs.access(directorio, (err) => {
+      if (err) {
+        console.log(chalk.red.inverse(`La ruta introducida ${directorio} no existe o es incorrecta`));
+      } else {
+        const rm = spawn(`rm`, [`-r`, directorio]);
+        rm.stdout.pipe(process.stdout);
+        console.log(`${directorio} ha sido eliminado correctamente`);
+      }
+    });
   }
 
-  comando6() {
-
+  comandoCp(origen: string, destino: string) {
+    fs.access(origen, (err) => {
+      if (err) {
+        console.log(chalk.red.inverse(`La ruta introducida ${origen} no existe o es incorrecta`));
+      } else {
+        const cp = spawn(`cp`, [`-r`, origen, destino]);
+        cp.stdout.pipe(process.stdout);
+        console.log(`${origen} ha sido copiado correctamente`);
+      }
+    });
   }
 }
 
 const prueba: App4 = new App4();
+
+yargs.command({
+  command: 'show',
+  describe: 'Dada una ruta concreta, mostrar si es un directorio o un fichero.',
+  builder: {
+    name: {
+      describe: 'Name',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler(argv) {
+    if (typeof argv.name === 'string' ) {
+      prueba.comandoShow(argv.name);
+    } else {
+      console.log(chalk.red("Hubo un error al introducir los comandos para añadir una nota"));
+    }
+  },
+});
+
+yargs.command({
+  command: 'mkdir',
+  describe: 'Crear un nuevo directorio a partir de una nueva ruta que recibe como parámetro.',
+  builder: {
+    name: {
+      describe: 'Name',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler(argv) {
+    if (typeof argv.name === 'string' ) {
+      prueba.comandoMkdir(argv.name);
+    } else {
+      console.log(chalk.red("Hubo un error al introducir los comandos para añadir una nota"));
+    }
+  },
+});
 
 
 yargs.command({
@@ -59,5 +146,68 @@ yargs.command({
     }
   },
 });
+
+yargs.command({
+  command: 'cat',
+  describe: 'Mostrar el contenido de un fichero',
+  builder: {
+    name: {
+      describe: 'Name',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler(argv) {
+    if (typeof argv.name === 'string' ) {
+      prueba.comandoCat(argv.name);
+    } else {
+      console.log(chalk.red("Hubo un error al introducir los comandos para añadir una nota"));
+    }
+  },
+});
+
+yargs.command({
+  command: 'rm',
+  describe: 'Borrar ficheros y directorios',
+  builder: {
+    name: {
+      describe: 'Name',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler(argv) {
+    if (typeof argv.name === 'string' ) {
+      prueba.comandoRm(argv.name);
+    } else {
+      console.log(chalk.red("Hubo un error al introducir los comandos para añadir una nota"));
+    }
+  },
+});
+
+yargs.command({
+  command: 'cp',
+  describe: 'Mover y copiar ficheros y/o directorios de una ruta a otra.',
+  builder: {
+    origen: {
+      describe: 'Origen del fichero/directorio a copiar',
+      demandOption: true,
+      type: 'string',
+    },
+    destino: {
+      describe: 'Destino del fichero/directorio a copiar',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler(argv) {
+    if (typeof argv.origen === 'string' && typeof argv.destino === 'string') {
+      prueba.comandoCp(argv.origen, argv.destino);
+    } else {
+      console.log(chalk.red("Hubo un error al introducir los comandos para añadir una nota"));
+    }
+  },
+});
+
 
 yargs.argv;
